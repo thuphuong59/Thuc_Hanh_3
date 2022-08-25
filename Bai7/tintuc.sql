620e3e7ac22d3f4a4b1ce1ba0ea49f8a912ab675
Imelda.tp <100985589+thuphuong59@users.noreply.github.com>
2022-08-25 15:03:35 +0700

    Add files via upload

---
+++ Bai7/tintuc.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 25, 2022 at 09:40 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tintuc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `theloai`
+--
+
+CREATE TABLE `theloai` (
+  `idTL` int(11) NOT NULL,
+  `TenTL` varchar(255) NOT NULL DEFAULT '',
+  `ThuTu` int(11) DEFAULT 0,
+  `AnHien` tinyint(1) DEFAULT 1,
+  `icon` varchar(255) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `theloai`
+--
+
+INSERT INTO `theloai` (`idTL`, `TenTL`, `ThuTu`, `AnHien`, `icon`) VALUES
+(12, 'Hai', 2, 1, 'WIN_20220212_14_40_03_Pro.jpg'),
+(13, 'vui', 5, 0, 'WIN_20220213_19_41_13_Pro.jpg'),
+(16, 'buon', 3, 1, 'WIN_20220522_08_49_07_Pro.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `theloai`
+--
+ALTER TABLE `theloai`
+  ADD PRIMARY KEY (`idTL`),
+  ADD UNIQUE KEY `TenTL` (`TenTL`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `theloai`
+--
+ALTER TABLE `theloai`
+  MODIFY `idTL` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
